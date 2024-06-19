7bd0c77b21d4674d7a310f2fb8d442b2700f380d
karimkashkoush <karimkashkoush5@gmail.com>
2024-06-19 14:40:13 +0300

    add

--- puplic/js/scribt.js
+++ puplic/js/scribt.js
@@ -2,7 +2,7 @@ function openCloseSidebar() {
     let sidebar = document.getElementById("sidebar")
     let sidebarSpans = document.querySelectorAll(".contact span")
     let openIcon = document.getElementById("openIcon")
-    
+
     if (sidebar.classList.contains("active")) {
         sidebar.classList.remove("active")
         sidebarSpans.forEach((ele) => {
@@ -21,3 +21,31 @@ function openCloseSidebar() {
         openIcon.classList.remove("fa-circle-left")
     }
 }
+
+// Form Validate
+
+function nationalityChange() {
+    let nationalitySelect = document.getElementById("nationality");
+    let nationalityInput = document.getElementById("nationalityInput")
+    if (nationalitySelect.value == "سعودي") {
+        nationalityInput.style.display = "none";
+        nationalityInput.value = "سعودي";
+    } else {
+        nationalityInput.value = "";
+        nationalityInput.style.display = "block";
+    }
+}
+
+function healthyChange() {
+    let healthySelect = document.getElementById("healthy");
+    let healthyInput = document.getElementById("healthyInput")
+    if (healthySelect.value == "لا") {
+        healthyInput.style.display = "none";
+        healthyInput.value = "لا";
+    } else {
+        healthyInput.value = "";
+        healthyInput.style.display = "block";
+    }
+}
+
+
